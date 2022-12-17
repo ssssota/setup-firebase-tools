00352af6eb4b61818546bd8129c8ca5e9244d916
ssssota <tommysouta@gmail.com>
2022-12-17 22:48:14 +0900

    fix: implement

--- src/main.ts
+++ src/main.ts
@@ -3,29 +3,39 @@ import * as tc from "@actions/tool-cache";
 import * as fs from "node:fs/promises";
 import * as os from "node:os";
 import * as path from "node:path";
+import { v4 as uuid } from "uuid";
 
 export async function run() {
 	const inputs = resolveInputs();
 	const platform = getPlatform();
 	const binUrl = getBinaryUrl(platform, inputs.version);
 	core.info(`Download binary from ${binUrl} ...`);
-	const binDir = await tc.downloadTool(binUrl);
-	core.info(`Complete download to ${binDir} ...`);
-	const [binFile] = await fs.readdir(path.dirname(binDir));
-	const binPath = path.join(binDir, binFile);
-	const scriptPath = path.join(
+	const tempBinPath = await tc.downloadTool(binUrl);
+	await fs.chmod(tempBinPath, "755");
+	const tempDir = path.dirname(tempBinPath);
+	core.info(`Complete download to ${tempBinPath} ...`);
+	const binDir = path.join(tempDir, uuid());
+	await fs.mkdir(binDir, { recursive: true });
+	const firebaseServiceAccount = path.join(binDir, "service-account.json");
+	if (inputs.firebaseServiceAccount) {
+		await fs.writeFile(firebaseServiceAccount, inputs.firebaseServiceAccount);
+		core.exportVariable(
+			"GOOGLE_APPLICATION_CREDENTIALS",
+			firebaseServiceAccount,
+		);
+	}
+	const binPath = path.join(
 		binDir,
-		"firebase", // platform === "win" ? "firebase.bat" : "firebase",
+		platform === "win" ? "firebase.exe" : "firebase",
 	);
-	core.info(`Create wrapper script to ${scriptPath} ...`);
-	const script = createScript(binPath, inputs.firebaseServiceAccount);
-	await fs.writeFile(scriptPath, script);
-	core.addPath(scriptPath);
+	await fs.rename(tempBinPath, binPath);
+	core.addPath(binDir);
 }
 
 export function resolveInputs() {
 	const version = core.getInput("version");
 	const firebaseServiceAccount = core.getInput("firebaseServiceAccount");
+	if (firebaseServiceAccount) core.setSecret(firebaseServiceAccount);
 	return {
 		version,
 		firebaseServiceAccount: firebaseServiceAccount || undefined,
@@ -33,8 +43,8 @@ export function resolveInputs() {
 }
 
 export function getPlatform() {
-	const plat = os.platform();
-	switch (plat) {
+	const platform = os.platform();
+	switch (platform) {
 		case "win32":
 			return "win";
 		case "darwin":
@@ -42,18 +52,9 @@ export function getPlatform() {
 		case "linux":
 			return "linux";
 	}
-	throw new Error(`Unsupported platform: ${plat}`);
+	throw new Error(`Unsupported platform: ${platform}`);
 }
 
 export function getBinaryUrl(platform: string, version: string) {
 	return `https://firebase.tools/bin/${platform}/${version}/`;
-}
-
-export function createScript(binPath: string, firebaseServiceAccount?: string) {
-	return [
-		"#!/bin/bash",
-		firebaseServiceAccount &&
-			`GOOGLE_APPLICATION_CREDENTIALS=${firebaseServiceAccount} \\`,
-		`${binPath} "$@"`,
-	].flatMap((line) => (typeof line === "string" ? [line] : []));
 }
